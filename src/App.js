f942552f167f303a8cfc022990d696fd551bac41
enej93 <enej.licina@windowslive.com>
2020-12-16 14:05:44 +0100

    added the color changing logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-      return (
-          <div className="react-root">
-              <div className="centered">
-                  <h1>Colour picker</h1>
-                  <button className="red">red</button>
-                  <button className="blue">blue</button>
-                  <button className="yellow">yellow</button>
-              </div>
-          </div>
+function App() {
+    const [color, setColor] = useState('')
+    console.log(color)
+    return (
+        <div className={"react-root " + color}>
+            <div className="centered">
+                <h1>Colour picker</h1>
+                <button className="red" onClick={() => setColor("red")}>red</button>
+                <button className="blue" onClick={() => setColor("blue")}>blue</button>
+                <button className="yellow" onClick={() => setColor("yellow")}>yellow</button>
+            </div>
+        </div>
     );
-  }
 }
 
 export default App;
